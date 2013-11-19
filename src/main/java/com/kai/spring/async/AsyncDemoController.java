e996c63fe213791eb0d95c9b7e349eec0d26d093
jones <jones@smartfun.com.tw>
2013-11-19 16:13:11 +0800

    test DeferredResult

--- src/main/java/com/kai/spring/async/AsyncDemoController.java
+++ src/main/java/com/kai/spring/async/AsyncDemoController.java
@@ -1,17 +1,23 @@
 package com.kai.spring.async;
 
 import java.io.Writer;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.async.DeferredResult;
@@ -23,6 +29,8 @@ public class AsyncDemoController {
 	Logger logger = Logger.getLogger(AsyncDemoController.class);
 	
 	private final Queue<DeferredResult<ModelAndView>> eventQueue = new ConcurrentLinkedQueue<DeferredResult<ModelAndView>>();
+	private final Map<String, Map<String, Object>> watchers = new ConcurrentHashMap<String, Map<String, Object>>();
+
 	
     @RequestMapping("/normal")
     public String normalCall(Model model) throws InterruptedException {
@@ -55,26 +63,65 @@ public class AsyncDemoController {
     }
  
     @RequestMapping("/deferred")
-    public DeferredResult<ModelAndView> deferredCall() {
-        DeferredResult<ModelAndView> result = new DeferredResult<ModelAndView>();
-        this.eventQueue.add(result);
+    public DeferredResult<ModelAndView> deferredCall(HttpSession session) throws InterruptedException {
+    	Map<String, Object> job = new HashMap<String, Object>();
+        DeferredResult<ModelAndView> result = new DeferredResult<ModelAndView>(10000L, "result");
+//        this.eventQueue.add(result);
+        job.put("deferredResult", result);
+     // this is simulate every request work cost time
+        job.put("workCostTime", Integer.valueOf((int)(Math.random()*10)));
+        String name = Thread.currentThread().getName();
+        job.put("requestThreadName", name);
+    	this.watchers.put(session.getId(), job);
+//    	String msg = String.format(Locale.getDefault(), "Session id %s ,Thread %s get request.", session.getId(), name);
+//		logger.debug(msg);
+		
         return result;
     }
  
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 2000)
     public void simulateExternalThread() throws InterruptedException {
-    	logger.debug("simulateExternalThread...");
-        Thread.sleep(2000);
-        for (DeferredResult<ModelAndView> result : this.eventQueue) {
+//    	logger.debug("----------------------...before work:"+new Date());
+//        Thread.sleep(10000);
+//        logger.debug("----------------------...after work:"+new Date());
+//        for (DeferredResult<ModelAndView> result : this.eventQueue) {
+//        	ModelAndView model = new ModelAndView("result");
+//        	long start = System.currentTimeMillis();
+//    		String name = Thread.currentThread().getName();
+//    		long duration = System.currentTimeMillis() - start;
+//    		String msg = String.format(Locale.getDefault(), "Thread %s completed the task in %d ms.", name, duration);
+//    		model.addObject("result", msg);
+//    		
+//            result.setResult(model);
+//            this.eventQueue.remove(result);
+//        }
+        
+        for (Map.Entry<String, Map<String, Object>> entry : watchers.entrySet()) {
+        	String sessionId = entry.getKey();
+        	Map<String, Object> map = entry.getValue();
+        	
+        	DeferredResult<ModelAndView> deferredResult = (DeferredResult<ModelAndView>) map.get("deferredResult");
+        	// this is simulate every request work cost time, but this is only one thread in this method
+        	// how to separate the work
+        	Integer workCostTime = (Integer) map.get("workCostTime"); 
+        	
+        	String requestThreadName = (String) map.get("requestThreadName");
+        	
         	ModelAndView model = new ModelAndView("result");
-        	long start = System.currentTimeMillis();
     		String name = Thread.currentThread().getName();
-    		long duration = System.currentTimeMillis() - start;
-    		String msg = String.format(Locale.getDefault(), "Thread %s completed the task in %d ms.", name, duration);
+    		String msg = String.format(Locale.getDefault(), "Session id %s , request Thread %s, work Thread %s, need work cost time %d sec.", sessionId, requestThreadName, name, workCostTime);
+    		logger.debug(msg);
     		model.addObject("result", msg);
     		
-            result.setResult(model);
-            this.eventQueue.remove(result);
+    		deferredResult.setResult(model);
+    		
+    		this.watchers.remove(sessionId);
         }
     }
+    
+    @ExceptionHandler
+	@ResponseBody
+	public String handleException(IllegalStateException ex) {
+		return "Handled exception: " + ex.getMessage();
+	}
 }
